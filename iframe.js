5577b4e1f85af9aefca196fcfa36fb009f56b40d
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-12-13 01:30:45 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,9 +1,4 @@
 const videoLinks = [
   // Replace these with your actual MP4 video URLs
-  "https://www.leaktube.org/media/videos/h264/16995_480p.mp4",
-  "https://www.leaktube.org/media/videos/h264/17261_720p.mp4",
-  "https://www.leaktube.org/media/videos/h264/17688_720p.mp4",
-  "https://www.leaktube.org/media/videos/h264/17961_480p.mp4",
-"https://www.leaktube.org/media/videos/h264/17832_480p.mp4",
-"https://www.leaktube.org/media/videos/h264/15925_720p.mp4"
+  "https://www.leaktube.org/media/videos/h264/16995_480p.mp4"
 ];
