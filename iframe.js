4317437717efc87c417b381424b6c3a3c81a491d
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-12-14 08:19:12 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,4 +1,4 @@
 const videoLinks = [
   // Replace these with your actual MP4 video URLs
-  "https://baddiesonly.tv/get_file/1/f18c70833e648d6096b7260fc12d1598/16000/16999/16999.mp4/?rnd=1765586211451"
+  "https://baddiesonly.tv/get_file/1/c2322d9c5e4da505646fecf31828f463/22000/22912/22912.mp4/?rnd=1765696608229"
 ];
